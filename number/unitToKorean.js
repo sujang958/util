41834d2e66bc620ab6e9d84016d51dc01f28036f
안꿀꿀 <63600304+MadeGOD@users.noreply.github.com>
2021-05-06 10:11:55 +0800

    edi : Number to Korean Bug Fixed

--- number/unitToKorean.js
+++ number/unitToKorean.js
@@ -1,50 +1,27 @@
-function num_to_kor(num) {
-    let bNum = BigInt(num);
-    let str = bNum.toString().replace('n', '');
-    let strArr = str.split('');
-    let resultArr = [];
-    let unitWords = {
-        '여백의미': null,
-        '': 3,
-        천: 3,
-        만: 4,
-        억: 4,
-        조: 4,
-        경: 4,
-        해: 4,
-        자: 4,
-        양: 4,
-        구: 4,
-        간: 4,
-        정: 4,
-        재: 4,
-        극: 4,
-        항아사: 4,
-        아승기: 4,
-        나유타: 4,
-        불가사의: 4
-    };
-    let final = [];
-    let a = 1;
-    let b = 1;
+function numberToKorean(number){
+    var inputNumber  = number < 0 ? false : number
+    var unitWords    = ['', '만', '억', '조', '경', '해', '자', '양', '구', '간', '정', '재', '극']
+    var splitUnit    = 10000
+    var splitCount   = unitWords.length
+    var resultArray  = []
+    var resultString = ''
 
-    for (let i = 3; i <= strArr.length; i += 3) {
-        let _arr = Object.entries(unitWords);
-        let __arr = _arr[i - (3 * a) + b];
-        a += 1;
-        b += 1;
-        if (!__arr)
-            continue;
-        if (str.substr(i, __arr[1]).length <= 0)
-            continue;
-        resultArr.push(str.substr(i-3, __arr[1]));
-    }
-    
-    for (let i in resultArr) {
-        let _arr = Object.entries(unitWords);
-        _arr = _arr.slice(1);
-        final.push(`${resultArr[i]}${_arr[i][0]}`);
+    for (var i = 0; i < splitCount; i++){
+        var unitResult = (inputNumber % Math.pow(splitUnit, i + 1)) / Math.pow(splitUnit, i)
+        unitResult = Math.floor(unitResult)
+        if (unitResult > 0){
+            resultArray[i] = unitResult
+        }
     }
 
-    return final.reverse().join(' ');
+    for (var a = 0; a < resultArray.length; a++){
+        if(!resultArray[a]) continue
+        resultString = String(resultArray[a]) + unitWords[a] + ' ' + resultString
+    }
+
+    return resultString.replace(/ $/, '')
 }
+/*
+ * @example numberToKorean(100000000)
+ * @output 1억
+ */
